c7aff618d70535cb9c5cbf5477e105553d61a621
Adarsh Gupta <adarshgupta664@gmail.com>
2025-09-08 16:04:32 +0530

    Drunk Alcoholic
    Faizal is very sad after finding out that he is responsible for Sardar's death. He tries to drown his sorrows in alcohol and gets very drunk. Now he wants to return home but is unable to walk straight. For every 
    3
    3 steps forward, he ends up walking one step backward.
    
    Formally, in the 
    1
    s
    t
    1 
    st
      second he moves 
    3
    3 steps forward, in the 
    2
    n
    d
    2 
    nd
      second he moves 
    1
    1 step backwards, in the 
    3
    r
    d
    3 
    rd
      second he moves 
    3
    3 steps forward, in 
    4
    t
    h
    4 
    th
      second 
    1
    1 step backwards, and so on.
    
    How far from his initial position will Faizal be after 
    k
    k seconds have passed? Assume that Faizal's initial position is 
    0
    0.

---
+++ Diff_874.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Diff_874{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0){
+            int x = sc.nextInt();
+            if(x%2==0){
+                System.out.println(x);
+            }else{
+                System.out.println(x+2);
+            }
+        }
+    }
+}
